ee63698bef116de771d8051d5c832db8713766df
iwodzu80 <iwodzu80@gmail.com>
2025-08-05 12:37:38 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,5 +4,5 @@
 export const SUPABASE_CONFIG = {
   PROJECT_ID: "fwqtztrfspbotzvpemkn",
   URL: "https://fwqtztrfspbotzvpemkn.supabase.co",
-  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cXR6dHJmc3Bib3R6dnBlbWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNTAyMjcsImV4cCI6MjA2MjYyNjIyN30.ccuumRIh9lFn_4QAJ67QsiIkKjPrvb8L4GDFx1-52YU"
+  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cXR6dHJmc3Bib3R6dnBlbWtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNTAyMjcsImV4cCI6MjA2MjYyNjIyN30.ccuumRIh9lFn_4QAJ67QsiIkKjPrvb8L4GDFx1-52YUM"
 } as const;
